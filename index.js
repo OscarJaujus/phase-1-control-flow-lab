da052f281807b746195d6feb3cd394238384d70a
Oscar Omondi <oscaromondiomondi@gmail.com>
2022-06-10 17:43:47 +0300

    commit tree 53c33dab8b2a461c9ae58968688cbae8368947d3

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-function scuberGreetingForFeet(distance){
+function scuberGreetingForFeet(distance) {
   // Write your code here!
-    if (distance <= 400)
+  if (distance <= 400)
     return "This one is on me!"
-    if (distance > 2500) 
+  if (distance > 2500)
     return "No can do."
-    if (distance > 2000) 
+  if (distance > 2000)
     return "I will gladly take your thirty bucks."
- 
+
 }
 
 
-function ternaryCheckCity(city){
+function ternaryCheckCity(city) {
   // Write your code here!
   if (city === "NYC")
-  return "Ok, sounds good."
-  else 
-  return "No go."
+    return "Ok, sounds good."
+  else (city === "Pitsburgh")
+    return "No go."
 
 
 }
 
-function switchOnCharmFromTip(tip){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
   if (tip === "generous")
-  return "Thank you so much."
+    return "Thank you so much."
   else if (tip !== "generous")
-  return "Thank you."
-  else
-  return "Bye."
+    return "Thank you."
+  else if (tip == "anything else")
+    return "Bye."
 }
